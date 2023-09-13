f77f249164ffb57bd71c41743bf8f83b7e83c640
Minhaj <minhajsinanvp@gmail.com>
2023-09-13 13:09:53 +0530

    Completed Service Section

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 // navbar scroll
 
 let nav = document.querySelector(".navbar")
@@ -27,10 +31,90 @@ window.onscroll = function(){
 
 document.addEventListener('click', function (event) {
     clsBtn = document.querySelector('.btn-close')
-    // Check if the clicked element has a data-bs-dismiss attribute with the value "offcanvas"
     if (event.target.classList[0] === "nav-link") {
-        // Find the Offcanvas element by its ID and close it
         clsBtn.click()
         
     }    
 });
+
+
+// scrollSpy
+
+
+
+// form validation
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+
+
+
+
+//   Animation section
+
+// Function to handle the intersection of the service cards
+// Function to handle the intersection of the service cards
+// Function to handle the animation of service cards
+// Function to handle the animation of service cards
+function animateServiceCards() {
+    const serviceCards = document.querySelectorAll('.service-card');
+    
+    serviceCards.forEach((card) => {
+        card.style.animation = 'fade-in 1s ease-in-out'; // Apply the animation
+        card.style.opacity = '0'; // Initially set opacity to 0
+    });
+}
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to handle scroll events
+function handleScroll() {
+    const serviceCards = document.querySelectorAll('.service-card');
+    
+    serviceCards.forEach((card) => {
+        if (isInViewport(card)) {
+            card.style.animationPlayState = 'running';
+            card.style.opacity = '1';
+        } else {
+            card.style.animationPlayState = 'paused';
+            card.style.opacity = '0';
+        }
+    });
+}
+
+// Add an event listener for the scroll event
+window.addEventListener('scroll', handleScroll);
+
+// Call the initial animation when the page loads
+window.addEventListener('load', animateServiceCards);
+
+
+
+
